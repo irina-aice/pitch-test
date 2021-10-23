310b7ad6842640628eb918a414dc2ab1f7d17fb1
irina-aice <61356164+irina-aice@users.noreply.github.com>
2021-10-23 18:52:29 +0300

    in progress

--- source/js/slider.js
+++ source/js/slider.js
@@ -2,12 +2,21 @@
 
 (function () {
   const slider = document.querySelector('.js-slider');
+  const sliderPagination = document.querySelector('.js-slider-pagination');
 
-  if (!slider) {
+  if (!slider || !sliderPagination) {
     return false;
   }
 
   new window.Swiper(slider, {
+    pagination: {
+      el: sliderPagination,
+      clickable: true,
+      type: 'fraction',
+      renderFraction: function(currentClass, totalClass) {
+        return `<span class="${currentClass}"></span> <span class="slider__divider">-</span> <span class="${totalClass}"></span>`;
+      },
+    }
     /*autoplay: {
       delay: 5000,
     }*/
